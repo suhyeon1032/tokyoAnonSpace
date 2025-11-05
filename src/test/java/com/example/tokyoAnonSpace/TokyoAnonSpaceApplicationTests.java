e13d7f4c424a96fe282625945147393a8de231c5
suhyeon1032 <suuh928@daum.net>
2025-11-05 15:31:18 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/tokyoAnonSpace/TokyoAnonSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tokyoAnonSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokyoAnonSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
